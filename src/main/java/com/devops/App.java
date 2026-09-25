c6884e226a30a62dcaff4131771175c39ab56cf8
Ajay Kumbhar <2022wb86460@wilp.bits-pilani.ac.in>
2026-09-25 14:47:57 +0530

    Modified application source code

--- src/main/java/com/devops/App.java
+++ src/main/java/com/devops/App.java
@@ -2,6 +2,6 @@ package com.devops;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello from Jenkins Continuous Integration!");
+        System.out.println("Hello from Jenkins Continuous Integration - Version 2");
     }
 }
